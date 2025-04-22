203bbfd800de0192612fbafbbd8a43b56211f649
devyaanshdwivedi <devyaanshdwivedi1234@gmail.com>
2025-04-22 14:42:54 +0530

    Shuffling - Generating Random Number

--- deck.go
+++ deck.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
-//Create a new type of 'deck' which is a slice of strings
+// Create a new type of 'deck' which is a slice of strings
 type deck []string
 
 //create a new function which belongs to the deck type
@@ -30,7 +32,7 @@ func newDeck() deck {
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card) // printing the index and the card
-		
+
 	}
 }
 
@@ -50,10 +52,20 @@ func newDeckFromFile(filename string) deck {
 	bs, err := ioutil.ReadFile(filename) // reading the file
 	if err != nil {
 		fmt.Println("Error:", err) // printing the error if any
-		os.Exit(1) // exiting the program with an error code
+		os.Exit(1)                 // exiting the program with an error code
 	}
 
 	//string(bs) // converting the byte slice to a string
 	s := strings.Split(string(bs), ",") // splitting the string into a slice of strings
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource((time.Now().UnixNano())) // creating a new source of randomness
+	r := rand.New(source)                             // creating a new random number generator
+
+	for i := range d { // iterating over the deck of cards
+		newPosition := r.Intn(len(d) - 1)           // generating a new random position
+		d[i], d[newPosition] = d[newPosition], d[i] // swapping the cards
+	}
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 //import "fmt"
 
 func main() {
-	cards := newDeckFromFile("my_cards")
-	cards.print() // printing the deck of cards
+	cards:= newDeck()
+	cards.shuffle() // shuffle the deck of cards
+	cards.print() // print the deck of cards
 }
